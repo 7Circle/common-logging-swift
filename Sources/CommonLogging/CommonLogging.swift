805b4ae0d0c8ae097543916b09f8c3369362515d
Michele Massaro <m.massaro@zero12.it>
2022-10-31 16:46:49 +0100

    Defined category as generic Enum of String

--- Sources/CommonLogging/CommonLogging.swift
+++ Sources/CommonLogging/CommonLogging.swift
@@ -1,3 +1,10 @@
+//                                     _      ___              _
+//     ___    ___      _ _    ___     / |    |_  )     o O O  | |_     ___    __ _    _ __
+//    |_ /   / -_)    | '_|  / _ \    | |     / /     o       |  _|   / -_)  / _` |  | '  \
+//   _/__|   \___|   _|_|_   \___/   _|_|_   /___|   TS__[O]  _\__|   \___|  \__,_|  |_|_|_|
+// _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| {======|_|"""""|_|"""""|_|"""""|_|"""""|
+// "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'./o--000'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'
+
 import Foundation
 import os
 
@@ -9,17 +16,11 @@ public struct CommonLogging {
         self.logger = logger
     }
     
-    public func logInfo(_ category: LogCategory, _ message: String) {
+    public func logInfo<E: RawRepresentable>(_ category: E, _ message: String) where E.RawValue == String {
         logger.info("[\(category.rawValue)] \(message)")
     }
 
-    public func logError(_ category: LogCategory, _ message: String) {
+    public func logError<E: RawRepresentable>(_ category: E, _ message: String) where E.RawValue == String {
         logger.error("[\(category.rawValue)] \(message)")
     }
-}
-
-public enum LogCategory: String {
-    case login = "Login"
-    case auth = "Auth"
-    case network = "Network"
 }
